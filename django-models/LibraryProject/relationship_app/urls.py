15b6e378e7c737dc9b7c8542c39abac16766ed6a
JohnBonge <rjbigango@gmail.com>
2025-07-25 16:47:05 +0200

    used Django built in LogoutView class

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
    
     path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register_view, name='register'),
 ]
