167d4ed5f7dee80d2800470269e90d9cc60c4136
JohnBonge <rjbigango@gmail.com>
2025-07-27 00:07:35 +0200

    implemented the custom user model in the bookshelf app

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    list_display = ['username', 'email', 'date_of_birth', 'is_staff']
+
+admin.site.register(CustomUser, CustomUserAdmi
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
 	list_display = ('title', 'author', 'publication_year')
