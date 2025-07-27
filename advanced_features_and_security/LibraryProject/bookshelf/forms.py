ab332d87a00e96c56c8055e5a11da83d0a4c051c
JohnBonge <rjbigango@gmail.com>
2025-07-27 23:43:56 +0200

    defined a class name ExampleForm in forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,4 +1,9 @@
 from django import forms
 
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
+
 class BookSearchForm(forms.Form):
     q = forms.CharField(required=False, max_length=100)
